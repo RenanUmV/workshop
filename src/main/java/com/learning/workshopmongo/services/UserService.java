5c8429d2d14a961ba8b82bb92ec5f37e55206723
RenanUmV <renan.uchoa.m@gmail.com>
2022-06-03 11:20:32 -0300

    Implements POST User method

--- src/main/java/com/learning/workshopmongo/services/UserService.java
+++ src/main/java/com/learning/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.learning.workshopmongo.domain.User;
+import com.learning.workshopmongo.dto.UserDTO;
 import com.learning.workshopmongo.repository.UserRepository;
 import com.learning.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 		
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
