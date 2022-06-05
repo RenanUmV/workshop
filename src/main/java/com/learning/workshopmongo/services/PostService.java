c5cacf7b38bf18cca972a54e0629bc75f15980e6
RenanUmV <renan.uchoa.m@gmail.com>
2022-06-05 11:28:16 -0300

    Simple consult with query methods

--- src/main/java/com/learning/workshopmongo/services/PostService.java
+++ src/main/java/com/learning/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.learning.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class PostService {
 		Optional<Post> obj = postRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 		
-	}	
+	}
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
