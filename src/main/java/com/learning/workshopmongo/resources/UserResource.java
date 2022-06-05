214f86d9812124c07e9dde4f382aabbb53290403
RenanUmV <renan.uchoa.m@gmail.com>
2022-06-04 23:08:04 -0300

    Endpoint to return the user posts

--- src/main/java/com/learning/workshopmongo/resources/UserResource.java
+++ src/main/java/com/learning/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.learning.workshopmongo.domain.Post;
 import com.learning.workshopmongo.domain.User;
 import com.learning.workshopmongo.dto.UserDTO;
 import com.learning.workshopmongo.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = userService.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
